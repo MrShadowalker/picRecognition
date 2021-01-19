2de7ea3502613b2906467dee612e8fd18095d031
MrShadowalker <mr.shadowalker@foxmail.com>
2021-01-19 12:37:45 +0800

    修改 oss access key

--- src/main/java/common/GlobalConstants.java
+++ src/main/java/common/GlobalConstants.java
@@ -1,7 +1,7 @@
 package common;
 
 public class GlobalConstants {
-    public static final String OSS_ACCESS_KEY_ID = "LTAIPWZ3JiLeoW7F";
-    public static final String OSS_ACCESS_KEY_SECRET = "Sn13Wfb20qqwNNHsB1KkIxUrIDvYCV";
+    public static final String OSS_ACCESS_KEY_ID = "access_key_id";
+    public static final String OSS_ACCESS_KEY_SECRET = "access_key_secret";
 
 }
