04be31d714e7ea51a432958ba93bcd33b274e782
MrShadowalker <mr.shadowalker@foxmail.com>
2021-01-19 15:49:30 +0800

    新增一些注释

--- src/main/java/common/GlobalConstants.java
+++ src/main/java/common/GlobalConstants.java
@@ -7,6 +7,8 @@ package common;
  */
 public class GlobalConstants {
 
+    private GlobalConstants() {}
+
     public static final String OSS_ACCESS_KEY_ID = "access_key_id";
     // 因为未来调用开始收费，请大家自行去阿里云开通账户进行授权
     //    public static final String OSS_ACCESS_KEY_ID = "LTAIPWZ3JiLeoW7F";
